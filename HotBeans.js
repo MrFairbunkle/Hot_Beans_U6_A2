fd592166022dd3a2e5e7f419eac1e532b5345285
MrFairbunkle <charliebhfw@icloud.com>
2024-03-07 16:14:13 +0000

    scrooll

--- HotBeans.js
+++ HotBeans.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var sections = document.querySelectorAll('section');
+    var navLinks = document.querySelectorAll('.nav-link');
 
+    function changeNavState() {
+      var index = sections.length;
+
+      while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+      navLinks.forEach((link) => link.classList.remove('active'));
+      navLinks[index].classList.add('active');
+    }
+
+    changeNavState();
+
+    window.addEventListener('scroll', changeNavState);
+  });
