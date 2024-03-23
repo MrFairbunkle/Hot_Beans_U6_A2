95e7a5008373a0a5e9ee57aacadb7a8fbaf5f6fa
MrFairbunkle <charliebhfw@icloud.com>
2024-03-23 14:01:06 +0000

    fcb

--- HotBeans.js
+++ HotBeans.js
@@ -37,14 +37,9 @@ window.addEventListener('scroll', function() {
 
   function myFunction() {
     var x = document.getElementById("myLinks");
-    var container = document.querySelector('.container');
-  
     if (x.style.display === "block") {
       x.style.display = "none";
-      container.classList.remove('change'); 
     } else {
       x.style.display = "block";
-      container.classList.add('change'); 
     }
   }
-  
